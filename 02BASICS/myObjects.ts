96159ceae84b4cd15eccb4b0f048856960479260
JoseMTrias <jmgarciatrias@gmail.com>
2023-04-25 13:03:19 +0200

    type Aliases

--- 02BASICS/myObjects.ts
+++ 02BASICS/myObjects.ts
@@ -1,23 +1,34 @@
-const User = {
-    name: "hi",
-    email: "hi@hi.com",
-    isActive: true
+// const User = {
+//     name: "hi",
+//     email: "hi@hi.com",
+//     isActive: true
+// }
+
+// function createUser({name: string, isPaid: boolean}) {}
+
+// let newUser = {name: "hi", isPaid: false, email: "hi@hi.com"}
+
+// createUser(newUser)
+
+
+// function createCourse():{name: string, price: number}{
+//     return {name: "reactjs", price: 234}
+// }
+
+
+// type Aliases
+
+type User = {
+    name: string;
+    email: string;
+    isActive: boolean
 }
 
-function createUser({name: string, isPaid: boolean}) {}
 
-let newUser = {name: "hi", isPaid: false, email: "hi@hi.com"}
-
-createUser(newUser)
-
-
-
-
-
-function createCourse():{name: string, price: number}{
-    return {name: "reactjs", price: 234}
+function createUser(user: User): User {
+    return {name: "", email: "", isActive: true}
 }
 
-
+createUser({name: "", email: "", isActive: true})
 
 export {}
