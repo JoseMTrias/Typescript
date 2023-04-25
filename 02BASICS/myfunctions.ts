19d25520cb379b74a32e7b3ef8523e450de7e39d
JoseMTrias <jmgarciatrias@gmail.com>
2023-04-25 11:59:28 +0200

    introduction to functions

---
+++ 02BASICS/myfunctions.ts
@@ -0,0 +1,19 @@
+function addTwo(num: number) {
+//   return num + 2;
+return "hello"
+}
+
+function getUpper(val: string) {
+    return val.toUpperCase()
+}
+
+function signUpUser(name: string, email: string, isPaid: boolean){}
+
+let loginUser = (name: string, email: string, isPaid: boolean = false) => {}
+
+let myValue = addTwo(5);
+getUpper("hello")
+signUpUser("hi", "there@there.com", false)
+loginUser("h", "h@a.com")
+
+export {};
