1b8867d7486e5c0373ea6c8d2b6bb9d22232e88a
JoseMTrias <jmgarciatrias@gmail.com>
2023-04-25 12:21:52 +0200

    more about functions

--- 02BASICS/myfunctions.ts
+++ 02BASICS/myfunctions.ts
@@ -1,19 +1,46 @@
-function addTwo(num: number) {
-//   return num + 2;
-return "hello"
+function addTwo(num: number): number {
+  return num + 2;
+  // return "hello"
 }
 
 function getUpper(val: string) {
-    return val.toUpperCase()
+  return val.toUpperCase();
 }
 
-function signUpUser(name: string, email: string, isPaid: boolean){}
+function signUpUser(name: string, email: string, isPaid: boolean) {}
 
-let loginUser = (name: string, email: string, isPaid: boolean = false) => {}
+let loginUser = (name: string, email: string, isPaid: boolean = false) => {};
 
 let myValue = addTwo(5);
-getUpper("hello")
-signUpUser("hi", "there@there.com", false)
-loginUser("h", "h@a.com")
+getUpper('hello');
+
+signUpUser('hi', 'there@there.com', false);
+loginUser('h', 'h@a.com');
+
+// function getValue(myVal: number): boolean {
+//     if (myVal > 5) {
+//         return true
+//     }
+//     return "200 OK"
+// }
+
+const getHello = (s: string): string => {
+  return 'whatever';
+};
+
+const heros = ['thor', 'spiderman', 'ironman'];
+// const heros = [1, 2, 3]
+
+heros.map((hero): string => {
+  return `hero is ${hero}`;
+});
+
+function consoleError(errmsg: string): void {
+  console.log(errmsg);
+}
+
+function handleError(errmsg: string): never {
+  throw new Error(errmsg);
+}
 
 export {};
