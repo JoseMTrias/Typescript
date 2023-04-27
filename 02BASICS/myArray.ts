f2c85a6a8c5554ba4a854308ae65877019e8fb40
JoseMTrias <jmgarciatrias@gmail.com>
2023-04-27 10:10:12 +0200

    Array

---
+++ 02BASICS/myArray.ts
@@ -0,0 +1,20 @@
+const bands: string[] = []
+const bandsMembers: number[] = []
+// const bandsMembers: Array<number> = []   different way
+
+type User = {
+    name: string
+    isActive: boolean
+}
+
+const allUsers: User[] = []
+
+const MLModels: number[][] = [
+    [255, 255, 255],
+    []
+]
+
+bands.push("idles")
+bandsMembers.push(4)
+
+allUsers.push({name: "hi", isActive: true})
