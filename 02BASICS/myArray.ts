b8a9a6addcd36dddea93f336d86e1ce6cebc7204
JoseMTrias <jmgarciatrias@gmail.com>
2023-04-27 10:20:05 +0200

    cleaning

--- 02BASICS/myArray.ts
+++ 02BASICS/myArray.ts
@@ -1,20 +1,17 @@
-const bands: string[] = []
-const bandsMembers: number[] = []
+const bands: string[] = [];
+const bandsMembers: number[] = [];
 // const bandsMembers: Array<number> = []   different way
 
 type User = {
-    name: string
-    isActive: boolean
-}
+  name: string;
+  isActive: boolean;
+};
 
-const allUsers: User[] = []
+const allUsers: User[] = [];
 
-const MLModels: number[][] = [
-    [255, 255, 255],
-    []
-]
+const MLModels: number[][] = [[255, 255, 255], []];
 
-bands.push("idles")
-bandsMembers.push(4)
+bands.push('idles');
+bandsMembers.push(4);
 
-allUsers.push({name: "hi", isActive: true})
+allUsers.push({ name: 'hi', isActive: true });
