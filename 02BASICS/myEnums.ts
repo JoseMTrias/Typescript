c1fa205b8150ee02cd20fb554d934d1531a8557a
JoseMTrias <jmgarciatrias@gmail.com>
2023-04-28 11:59:56 +0200

    Enums

---
+++ 02BASICS/myEnums.ts
@@ -0,0 +1,19 @@
+enum SeatChoice {
+    aisle = "aisle",
+    middle = 3,
+    window,
+    fourth
+}
+
+const hcSeat = SeatChoice.aisle
+
+//adding const before enum 
+
+// const enum SeatChoice {
+//     aisle = "aisle",
+//     middle = 3,
+//     window,
+//     fourth
+// }
+
+// const hcSeat = SeatChoice.aisle
