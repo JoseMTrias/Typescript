1830d4756e2d0b170c766203f4314f47ec2cbcdc
JoseMTrias <jmgarciatrias@gmail.com>
2023-05-08 11:58:27 +0200

    private and public

--- 03PURETS/dist/index.js
+++ 03PURETS/dist/index.js
@@ -1,10 +1,19 @@
 "use strict";
+// class User {
+//   public email: string;
+//   private name: string; //you can also use # for private
+//   readonly city: string = 'sevilla';
+//   constructor(email: string, name: string) {
+//     this.email = email;
+//     this.name = name;
+//   }
+// }
 class User {
     constructor(email, name) {
-        this.city = 'sevilla';
         this.email = email;
         this.name = name;
+        this.city = 'sevilla';
     }
 }
 const jose = new User('j@t.com', 'jose');
-// jose.city = "sev"
+// jose.name;
--- 03PURETS/src/index.ts
+++ 03PURETS/src/index.ts
@@ -1,12 +1,21 @@
+// class User {
+//   public email: string;
+//   private name: string; //you can also use # for private
+//   readonly city: string = 'sevilla';
+//   constructor(email: string, name: string) {
+//     this.email = email;
+//     this.name = name;
+//   }
+// }
+
 class User {
-  email: string;
-  name: string;
   readonly city: string = 'sevilla';
-  constructor(email: string, name: string) {
-    this.email = email;
-    this.name = name;
-  }
+  constructor(
+    public email: string,
+    public name: string
+  ) // private userId: string
+  {}
 }
 
 const jose = new User('j@t.com', 'jose');
-// jose.city = "sev"
+// jose.name;
