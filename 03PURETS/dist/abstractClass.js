08e692a9fa1dbf613505e0625ac624844bdffb15
JoseMTrias <jmgarciatrias@gmail.com>
2023-05-08 13:57:27 +0200

    abstract class

---
+++ 03PURETS/dist/abstractClass.js
@@ -0,0 +1,24 @@
+"use strict";
+class TakePhoto {
+    constructor(cameraMode, filter) {
+        this.cameraMode = cameraMode;
+        this.filter = filter;
+    }
+    getReelTime() {
+        //some complex calculation
+        return 12;
+    }
+}
+class Instagram extends TakePhoto {
+    constructor(cameraMode, filter, burst) {
+        super(cameraMode, filter);
+        this.cameraMode = cameraMode;
+        this.filter = filter;
+        this.burst = burst;
+    }
+    getSepia() {
+        console.log('sepia');
+    }
+}
+const manolo = new Instagram('illo', 'que', 4);
+manolo.getReelTime();
