d8b3f817fdcc674374425e666ec2ede27efc6d11
JoseMTrias <jmgarciatrias@gmail.com>
2023-05-08 15:31:29 +0200

    Discriminated Union and Exhaustiviness Checking with never

--- 03PURETS/src/detection.ts
+++ 03PURETS/src/detection.ts
@@ -53,7 +53,7 @@ function logValue(x: Date | string) {
 type Fish = { swim: () => void };
 type Bird = { fly: () => void };
 
-function isFish(pet: Fish | Bird) : pet is Fish{
+function isFish(pet: Fish | Bird): pet is Fish {
   return (pet as Fish).swim !== undefined;
 }
 
@@ -64,5 +64,47 @@ function getFood(pet: Fish | Bird) {
   } else {
     pet;
     return 'bird food';
+  }
+}
+
+interface Circle {
+  kind: 'circle';
+  radius: number;
+}
+
+interface Square {
+  kind: 'square';
+  side: number;
+}
+
+interface Rectangle {
+  kind: 'rectangle';
+  length: number;
+  width: number;
+}
+
+type Shape = Circle | Square | Rectangle;
+
+function getTrueShape(shape: Shape) {
+  if (shape.kind === 'circle') {
+    return Math.PI * shape.radius ** 2;
+  }
+  //   return shape.side * shape.side;
+}
+
+function getArea(shape: Shape) {
+  switch (shape.kind) {
+    case 'circle':
+      return Math.PI * shape.radius ** 2;
+
+    case 'square':
+      return shape.side * shape.side;
+
+    case 'rectangle':
+      return shape.length * shape.width;
+
+    default:
+      const _defaultForShape: never = shape;
+      return _defaultForShape;
   }
 }
