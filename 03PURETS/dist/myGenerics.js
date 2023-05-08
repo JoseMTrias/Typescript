68bb112980ba13467a20655be8881783cb9fce25
JoseMTrias <jmgarciatrias@gmail.com>
2023-05-08 14:12:57 +0200

    generics

---
+++ 03PURETS/dist/myGenerics.js
@@ -0,0 +1,18 @@
+"use strict";
+const score = [];
+const names = [];
+function identityOne(val) {
+    return val;
+}
+function identityTwo(val) {
+    return val;
+}
+function identityThree(val) {
+    return val;
+}
+// identityThree(true)
+// different way of doing it, shorter
+function identityFour(val) {
+    return val;
+}
+// identityFour<Bootle>({})
