e7d11abe9145872d9b892748eb3003b38409fc2a
JoseMTrias <jmgarciatrias@gmail.com>
2023-05-08 14:58:30 +0200

    Type Narrowing

---
+++ 03PURETS/dist/detection.js
@@ -0,0 +1,26 @@
+"use strict";
+function detectType(val) {
+    if (typeof val === 'string') {
+        return val.toLocaleLowerCase();
+    }
+    return val + 3;
+}
+function provideId(id) {
+    if (!id) {
+        console.log('provide id');
+        return;
+    }
+    id.toLocaleLowerCase();
+}
+function printAll(strs) {
+    if (strs) {
+        if (typeof strs === 'object') {
+            for (const s of strs) {
+                console.log(s);
+            }
+        }
+        else if (typeof strs === 'string') {
+            console.log(strs);
+        }
+    }
+}
