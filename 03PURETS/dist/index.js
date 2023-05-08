5c070412884a4162ac5cfad1b92c466b276b148d
JoseMTrias <jmgarciatrias@gmail.com>
2023-05-08 12:17:11 +0200

    getters and setters

--- 03PURETS/dist/index.js
+++ 03PURETS/dist/index.js
@@ -9,11 +9,29 @@
 //   }
 // }
 class User {
-    constructor(email, name) {
+    constructor(email, name // private userId: string
+    ) {
         this.email = email;
         this.name = name;
+        this._courseCount = 1;
         this.city = 'sevilla';
+    }
+    deleteToken() {
+        console.log('Token deleted');
+    }
+    get getAppleEmail() {
+        return `apple${this.email}`;
+    }
+    get courseCount() {
+        return this._courseCount;
+    }
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error('Course count should be more than 1');
+        }
+        this._courseCount = courseNum;
     }
 }
 const jose = new User('j@t.com', 'jose');
 // jose.name;
+// jose.deleteToken()
