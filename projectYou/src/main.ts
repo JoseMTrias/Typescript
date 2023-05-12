4a83583ac1a484bdc45d5262998c4bf266764f8b
JoseMTrias <jmgarciatrias@gmail.com>
2023-05-12 11:38:22 +0200

    finishing main ts

--- projectYou/src/main.ts
+++ projectYou/src/main.ts
@@ -4,7 +4,43 @@ import ListItem from './model/ListItem';
 import ListTemplate from './templates/ListTemplates';
 
 const initApp = (): void => {
+  const fullList = FullList.instance;
+  const template = ListTemplate.instance;
 
-}
+  const itemEntryForm = document.getElementById(
+    'itemEntryForm'
+  ) as HTMLFormElement;
 
-document.addEventListener("DOMContentLoaded", initApp)
+  itemEntryForm.addEventListener('submit', (event: SubmitEvent): void => {
+    event.preventDefault();
+
+    const input = document.getElementById('newItem') as HTMLInputElement;
+
+    const newEntryText: string = input.value.trim();
+
+    if (!newEntryText.length) return;
+
+    const itemId: number = fullList.list.length
+      ? parseInt(fullList.list[fullList.list.length - 1].id) + 1
+      : 1;
+
+    const newItem = new ListItem(itemId.toString(), newEntryText);
+
+    fullList.addItem(newItem);
+    template.render(fullList);
+  });
+
+  const clearItems = document.getElementById(
+    'clearItemsButton'
+  ) as HTMLButtonElement;
+
+  clearItems.addEventListener('click', (): void => {
+    fullList.clearList();
+    template.clear();
+  });
+
+  fullList.load();
+  template.render(fullList);
+};
+
+document.addEventListener('DOMContentLoaded', initApp);
